7b55db273baa7cc1729c9c3f2f4d5f6be5900e66
afa-abraham <afacebu.abraham@gmail.com>
2024-11-28 22:05:12 +0800

    lielie

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from 'vite-plugin-commonjs';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), commonjs()],
+  optimizeDeps: {
+    include: [
+      'hoist-non-react-statics', 
+      'react',
+      'react-dom',
+    ],
+  },
+});
